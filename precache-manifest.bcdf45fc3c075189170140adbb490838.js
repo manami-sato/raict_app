3ba3a7370c69f34b7fd7fd62b58955651efb4d18
sato manami <manamisato2001@gmail.com>
2022-06-09 14:23:20 +0900

    commit

---
+++ precache-manifest.bcdf45fc3c075189170140adbb490838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6600b90961c4c6b3e7c673878ae0cd8f",
+    "url": ".htaccess"
+  },
+  {
+    "revision": "193bfd05c8f19da47d2e",
+    "url": "css/app.01f74855.css"
+  },
+  {
+    "revision": "6bea1001d3b44140b08ee1df5bc45cb3",
+    "url": "index.html"
+  },
+  {
+    "revision": "193bfd05c8f19da47d2e",
+    "url": "js/app.6f43a063.js"
+  },
+  {
+    "revision": "b70aa5adbb3397ff99e7",
+    "url": "js/chunk-vendors.c54dbbe1.js"
+  },
+  {
+    "revision": "a0880ee5dbb4b26b23fcbe50f192af4d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
